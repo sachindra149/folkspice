e31ba498ea278ae03de37625bf14ebafd8dae3f5
Sachindra <sachindra149@gmail.com>
2023-04-28 21:55:23 +0530

    Initial Commit

---
+++ dist/about-us.aa69868b.js
@@ -0,0 +1,19 @@
+$(function() {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+$("button.navbar-toggler").click(function() {
+    $(this).hasClass("open") ? $(this).removeClass("open") : $(this).addClass("open");
+});
+$(".year").text(new Date().getFullYear());
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+    myFunction();
+};
+function myFunction() {
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = winScroll / height * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+//# sourceMappingURL=about-us.aa69868b.js.map
---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+});
+$("button.navbar-toggler").click(function () {
+    $(this).hasClass("open")
+        ? $(this).removeClass("open")
+        : $(this).addClass("open");
+});
+$(".year").text(new Date().getFullYear());
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+    myFunction();
+};
+
+function myFunction() {
+    var winScroll =
+        document.body.scrollTop || document.documentElement.scrollTop;
+    var height =
+        document.documentElement.scrollHeight -
+        document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("myBar").style.width = scrolled + "%";
+}
